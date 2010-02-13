22a0846f58ecd00e16e25ee51839edb264562ea9
rollxx <gravatar@rollxx.com>
2010-02-14 00:15:09 +0100

    split the lexer and parser grammars into separate files

--- runsparql.php
+++ runsparql.php
@@ -5,9 +5,9 @@ set_include_path(get_include_path() . PATH_SEPARATOR . '/Applications/MAMP/htdoc
 set_include_path(get_include_path() . PATH_SEPARATOR . '/Applications/MAMP/htdocs/rollxx-sparql2-parser/ontowiki/src/libraries/');
 // ini_set("memory_limit","1G");
 $time_start = microtime(true);
-require_once '../antlr-php-runtime/runtime/Php/antlr.php';
+require_once '/Users/roll/Downloads/antlrphpruntime-read-only/runtime/Php/antlr.php';
 require_once 'Erfurt_Sparql_Sparql10Lexer.php';
-//require_once 'Erfurt_Sparql_Sparql11_Tokens.php';
+require_once 'Erfurt_Sparql_Sparql10_Tokens.php';
 require_once 'Erfurt_Sparql_Sparql10Parser.php';
 $include_time_end = microtime(true);
 
@@ -24,13 +24,13 @@ $lexer = new Erfurt_Sparql_Sparql10Lexer($input);
 // $lexer = new CommonLexer($input);
 $tokens = new CommonTokenStream($lexer);
 
-foreach ($tokens->getTokens() as $t) {
-		echo $t."\n";
-}
+//foreach ($tokens->getTokens() as $t) {
+//		echo $t."\n";
+//}
 
-// $parser = new Erfurt_Sparql_Sparql10Parser($tokens);
-// $q = $parser->start();
-// var_dump($q->getSparql());
+$parser = new Erfurt_Sparql_Sparql10Parser($tokens);
+$q = $parser->start();
+var_dump($q->getSparql());
 $time_end = microtime(true);
 $time = $time_end - $time_start;
 
